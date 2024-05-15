006d94b3256bcfcb093c8683bc5ca9c80e3fc9ae
MatiasCeb <60543600+MatiasCeb@users.noreply.github.com>
2024-05-15 08:45:09 -0300

    proyect base

---
+++ src/main/java/com/microservicios/pacientes/model/Paciente.java
@@ -0,0 +1,37 @@
+package com.microservicios.pacientes.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter @Setter
+@Entity
+public class Paciente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idPaciente;
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private Date fechaNac;
+    private String telefono;
+
+    public Paciente() {
+
+    }
+
+    public Paciente(Long idPaciente, String dni, String nombre, String apellido, Date fechaNac, String telefono) {
+        this.idPaciente = idPaciente;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNac = fechaNac;
+        this.telefono = telefono;
+    }
+}
---
+++ src/main/java/com/microservicios/pacientes/service/IPacienteService.java
@@ -0,0 +1,18 @@
+package com.microservicios.pacientes.service;
+
+import com.microservicios.pacientes.model.Paciente;
+
+import java.util.List;
+
+public interface IPacienteService {
+    public List<Paciente> getPacientes();
+
+    public void savePaciente(Paciente pac);
+
+    public void deletePaciente(Long id);
+
+    public Paciente findPaciente(Long id);
+
+    public void editPaciente(Paciente pac);
+
+}
